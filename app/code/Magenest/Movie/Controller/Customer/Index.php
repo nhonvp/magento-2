e58787af441c9689dff16f5fcf30e0a55566cf4a
nhon <nhonpham13@gmail.com>
2021-07-09 09:19:40 +0700

    test eav1

--- app/code/Magenest/Movie/Controller/Customer/Index.php
+++ app/code/Magenest/Movie/Controller/Customer/Index.php
@@ -5,11 +5,9 @@ namespace Magenest\Movie\Controller\Customer;
 class Index extends \Magento\Framework\App\Action\Action
 {
     public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Customer\Model\CustomerFactory $customerFactory,
+        \Magento\Framework\App\Action\Context $context
     ) {
         parent::__construct($context);
-        $this->customerFactory = $customerFactory;
     }
     public function execute()
     {
